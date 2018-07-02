a6ba4f13014edcea1397ac8715922cf6e0aeefc4
Neo <>
2018-07-02 20:38:34 +0200

    SW.js, Apollo Persist/InMemory, Manifest.json + Image/Icons

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+
+
+const cache = new InMemoryCache({});
+
+persistCache({
+  cache,
+  storage: window.localStorage,
+  debug:true
+});
+
 
 const client = new ApolloClient({
-    uri: 'https://flexigraphql2.herokuapp.com/graphql',
+    uri: 'https://flexigraphql2.herokuapp.com/graphql', cache,
     request: async (operation) => {
         const username = "weingaen";
         const intrapassword = "978c447b32798766c3f1d79b3c75cd1c"
